77596f4d509e57348601755ba193b37b5f046917
Abdul-Ahad-Ali <210904@students.au.edu.pk>
2024-03-12 22:32:30 +0500

    Amazon Buy Now Button Added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,16 +2,17 @@ import React from 'react';
 import BookCard from './BookCard'; // Importing the BookCard component
 import '../styles/BookCard.scss';
 import '../styles/BookList.scss';
+
 const BookList = ({ books }) => {
-    return (
-        <div className="book-list-container">
-            {books.map((book, index) => (
-                <div key={index} className="book-card-wrapper">
-                    <BookCard book={book} />
-                </div>
-            ))}
+  return (
+    <div className="book-list-container">
+      {books.map((book, index) => (
+        <div key={index} className="book-card-wrapper">
+          <BookCard book={book}/>  {/* Pass averageRating prop */}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default BookList;
